10af557bc255a868f5c3f3f5c69244f91e155c0e
chutki-25 <sirikollur25@gmail.com>
2019-07-22 15:51:50 +0530

    Solutions to assignment 1

--- M1_Q/q10.py
+++ M1_Q/q10.py
@@ -1,2 +1,13 @@
 ''' Program to print the pattern'''
-n=int(input("Enter n:"))
+
+max = 4
+
+for i in range ( 1, max + 1 ):
+    for k in range(max,i,-1):
+        print(" ",end="")
+    for l in range(1, i + 1):
+        print(l,end="")
+    for m in range(l-1,0,-1):
+        print(m,end="")
+    print()
+
